fd9159eba92a74f77225979b284168df3dff8e16
katesilatory <katesilatory@gmail.com>
2022-11-23 23:09:38 +0800

    updates

---
+++ src/components/Columns.js
@@ -0,0 +1,23 @@
+export const Columns = [
+    {
+        Header: 'Date'
+    },
+    {
+        Header: 'Time In'
+    },
+    {
+        Header: 'Time Out'
+    },
+    {
+        Header: 'Student ID'
+    },
+    {
+        Header: 'Name'
+    },
+    {
+        Header: 'Course'
+    },
+    {
+        Header: 'GSFE'
+    }
+]
